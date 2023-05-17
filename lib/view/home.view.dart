e785868e26438feff2414261e7f5663e71c4c479
Fabrizio Diaz <fabriziojdn02@gmail.com>
2023-05-17 17:47:45 -0500

    added categories UI

--- lib/view/home.view.dart
+++ lib/view/home.view.dart
@@ -15,28 +15,169 @@ class HomeView extends StatelessWidget {
       backgroundColor: Colors.grey[100],
       body: SafeArea(
           child: Column(
-        children: [
-          const Header(),
-          const SizedBox(
+        children: const [
+          Header(
+            imgUser: 'assets/images/default.png',
+          ),
+          SizedBox(
             height: 50,
           ),
-          // ignore: sized_box_for_whitespace
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const TitleWithCustomUnderline(text: "Bienvenido"),
-                IconButton(
-                    iconSize: 35.0,
-                    onPressed: () {},
-                    color: Colors.grey[800],
-                    icon: const Icon(Icons.accessibility_new)),
-              ],
-            ),
-          )
+          WelcomeMessageContainer(),
+          CategoriesListScroll(),
+          TrendingMessage()
         ],
       )),
+    );
+  }
+}
+
+class CategoriesListScroll extends StatelessWidget {
+  const CategoriesListScroll({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.only(right: 15.0),
+        child: Row(children: [
+          CategoryCard(
+              categoryTitle: "Favoritos",
+              img: "assets/images/favoritesLogo.jpg",
+              press: () {}),
+          CategoryCard(
+              categoryTitle: "Odiados",
+              img: "assets/images/hatedLogo.jpg",
+              press: () {}),
+          CategoryCard(
+              categoryTitle: "Deseos",
+              img: "assets/images/wishLogo.jpg",
+              press: () {})
+        ]),
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    Key? key,
+    required this.categoryTitle,
+    required this.press,
+    required this.img,
+  }) : super(key: key);
+
+  final String categoryTitle, img;
+  final VoidCallback? press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: const EdgeInsets.only(left: 20, top: 20 / 2, bottom: 10 * 2),
+        width: MediaQuery.of(context).size.width * 0.4,
+        height: MediaQuery.of(context).size.width * 0.6,
+        // decoration: BoxDecoration(
+        //     borderRadius: BorderRadius.circular(40.0),
+        //     color: const Color.fromRGBO(165, 195, 223, 1)),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40.0),
+          color: const Color.fromRGBO(165, 195, 223, 1),
+          image: DecorationImage(
+            image: AssetImage(img),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black
+                  .withOpacity(0.3), // Ajusta la opacidad aquí (0.0 - 1.0)
+              BlendMode
+                  .darken, // Puedes cambiar el modo de fusión según tus necesidades
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Mis",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                categoryTitle,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomeMessageContainer extends StatelessWidget {
+  const WelcomeMessageContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "Bienvenido",
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: kPrimaryColor),
+          ),
+          IconButton(
+              iconSize: 35.0,
+              onPressed: () {},
+              color: kPrimaryColor,
+              icon: const Icon(Icons.accessibility_new)),
+        ],
+      ),
+    );
+  }
+}
+
+class TrendingMessage extends StatelessWidget {
+  const TrendingMessage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: const [
+          Text(
+            "Viajes de Moda",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: kPrimaryColor),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -44,12 +185,13 @@ class HomeView extends StatelessWidget {
 class Header extends StatelessWidget {
   const Header({
     super.key,
+    required this.imgUser,
   });
-
+  final String imgUser;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 1.0, right: 5.0, top: 10.0),
+      padding: const EdgeInsets.only(left: 1.0, right: 8.0, top: 10.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -71,41 +213,10 @@ class Header extends StatelessWidget {
             image: AssetImage("assets/images/mainLogo.png"),
             width: 50,
           ),
-          const CircleAvatar(
+          CircleAvatar(
             radius: 20,
-            backgroundImage: AssetImage('assets/images/default.png'),
+            backgroundImage: AssetImage(imgUser),
           )
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithCustomUnderline extends StatelessWidget {
-  const TitleWithCustomUnderline({
-    Key? key,
-    required this.text,
-  }) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    // ignore: sized_box_for_whitespace
-    return Container(
-      height: 24,
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0 / 4),
-            child: Text(
-              text,
-              style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: kPrimaryColor),
-            ),
-          ),
         ],
       ),
     );
