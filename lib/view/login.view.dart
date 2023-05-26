076753cd4430a2e74636b9d0e61938446497c812
Fabrizio Diaz <fabriziojdn02@gmail.com>
2023-05-26 00:26:23 -0500

    firabase firestore

--- lib/view/login.view.dart
+++ lib/view/login.view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:first_app_flutter/Routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -12,18 +13,23 @@ class LoginView extends StatefulWidget {
 
 class _LoginViewState extends State<LoginView> {
   late final LocalAuthentication auth;
-  late bool _supportState = false;
+  final TextEditingController _idController = TextEditingController();
   @override
   void initState() {
     super.initState();
     auth = LocalAuthentication();
-    auth.isDeviceSupported().then((bool isSupported) => setState(() {
-          _supportState = isSupported;
-        }));
   }
 
   Future<void> _authenticate() async {
     try {
+      final db = FirebaseFirestore.instance;
+      final id = _idController.text;
+      final documentRef = db.collection("Users").doc(id);
+      bool documentExists =
+          await documentRef.get().then((snapshot) => snapshot.exists);
+      if (!documentExists) {
+        await documentRef.set({});
+      }
       bool authenticated = await auth.authenticate(
           localizedReason:
               "Scan your fingerprint (or face or whatever) to authenticate",
@@ -32,6 +38,7 @@ class _LoginViewState extends State<LoginView> {
 
       // ignore: avoid_print
       print("Authenticated : $authenticated");
+
       if (authenticated) {
         // ignore: use_build_context_synchronously
         Navigator.pushNamed(context, Routes.Homepage);
@@ -42,17 +49,6 @@ class _LoginViewState extends State<LoginView> {
     }
   }
 
-  Future<void> _getAvailableBiometrics() async {
-    List<BiometricType> availableBiometrics =
-        await auth.getAvailableBiometrics();
-    // ignore: avoid_print
-    print("List of availableBiometrics : $availableBiometrics");
-
-    if (!mounted) {
-      return;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -60,90 +56,105 @@ class _LoginViewState extends State<LoginView> {
       backgroundColor: const Color(0xFF3C3E52),
       body: Padding(
         padding: const EdgeInsets.all(18.0),
-        child: Column(children: [
-          const Center(
-            child: Text(
-              "EssaDiabetes",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          // ignore: avoid_unnecessary_containers
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 50.0),
-            child: Column(children: [
-              if (_supportState)
-                const Text("this device is supported")
-              else
-                const Text("this device is not supported"),
-              Image.asset(
-                "assets/images/fingerprint.png",
-                width: 150,
-                color: Colors.lightBlue[400],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                "Autenticación por Huella",
+        child: SingleChildScrollView(
+          child: Column(children: [
+            const Center(
+              child: Text(
+                "EssaDiabetes",
                 style: TextStyle(
                     color: Colors.white,
-                    fontSize: 23,
+                    fontSize: 50,
                     fontWeight: FontWeight.bold),
               ),
-              // ignore: sized_box_for_whitespace
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 15.0),
-                width: 140,
-                child: const Text(
-                  "Autenticación usando la huella digital como metodo de acceso a la apliación",
-                  textAlign: TextAlign.center,
+            ),
+            // ignore: avoid_unnecessary_containers
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 50.0),
+              child: Column(children: [
+                Image.asset(
+                  "assets/images/fingerprint.png",
+                  width: 150,
+                  color: Colors.lightBlue[400],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Autenticación por Huella",
                   style: TextStyle(
-                    color: Colors.white,
-                    height: 1.5,
-                  ),
+                      color: Colors.white,
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold),
                 ),
-              ),
-              // ignore: sized_box_for_whitespace
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 15.0),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.lightBlue[400],
-                    borderRadius: BorderRadius.circular(20.0)),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 4.0, bottom: 8.0),
-                  child: TextButton(
-                    onPressed: _getAvailableBiometrics,
-                    child: const Text(
-                      "Get availableBiometrics",
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+                // ignore: sized_box_for_whitespace
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 15.0),
+                  width: 140,
+                  child: const Text(
+                    "Autenticación usando la huella digital como metodo de acceso a la apliación",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      height: 1.5,
                     ),
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 15.0),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.lightBlue[400],
-                    borderRadius: BorderRadius.circular(20.0)),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 4.0, bottom: 8.0),
-                  child: TextButton(
-                    onPressed: _authenticate,
-                    child: const Text(
-                      "Ingresar",
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+                // ignore: sized_box_for_whitespace
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 15.0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.lightBlue[400],
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 4.0, bottom: 8.0),
+                    child: TextButton(
+                      onPressed: _authenticate,
+                      child: const Text(
+                        "Ingresar",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ]),
-          )
-        ]),
+                // ignore: avoid_unnecessary_containers
+                Container(
+                    child: TextField(
+                  controller: _idController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter
+                        .digitsOnly, // Acepta solo dígitos
+                    LengthLimitingTextInputFormatter(
+                        8), // Limita la longitud máxima a 8 caracteres
+                  ],
+                  style: const TextStyle(
+                    color: Colors.white, // Cambia el color del texto a blanco
+                  ),
+                  textAlign: TextAlign.center, // Centra el texto ingresado
+                  decoration: const InputDecoration(
+                    labelText: 'Ingrese su DNI',
+                    labelStyle: TextStyle(
+                      color: Colors.white, // Cambia el color del label a blanco
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors
+                            .white, // Cambia el color del borde cuando no está enfocado
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors
+                            .white, // Cambia el color del borde cuando está enfocado
+                      ),
+                    ),
+                  ),
+                ))
+              ]),
+            )
+          ]),
+        ),
       ),
     ));
   }
